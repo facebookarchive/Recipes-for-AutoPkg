7fb07b924b9745577ad06b1bbd3a49322c74aa56
Nick McSpadden <nmcspadden@gmail.com>
2019-04-24 15:00:06 -0700

    Adding Xcode downloader

---
+++ Xcode/AppleDataGatherer.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+#
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+#
+"""Get all Version information from Xcode."""
+
+
+# Disabling warnings for env members and imports that only affect recipe-
+# specific processors.
+# pylint: disable=e1101,f0401
+
+import os
+from urllib import quote
+
+from autopkglib import Processor, ProcessorError
+
+__all__ = ["AppleDataGatherer"]
+
+
+class AppleDataGatherer(Processor):
+    """Gather Xcode-specific curl data payloads into a file on disk."""
+
+    description = __doc__
+    input_variables = {
+        "apple_id": {
+            "required": True,
+            "description": ("AppleID that can log into the Apple dev portal."),
+        },
+        "password": {
+            "required": True,
+            "description": ("Password for AppleID that can log into Apple dev portal."),
+        },
+        "appID_key": {"required": True, "description": ("App ID key to log into.")},
+    }
+    output_variables = {"data_pathname": {"description": "Path to the data file."}}
+
+    __doc__ = description
+
+    def main(self):
+        """Store the login data file."""
+        appleIDstring = "appleId={}&".format(quote(self.env["apple_id"]))
+        appIDKeystring = "appIdKey={}&".format(self.env["appID_key"])
+        passwordstring = "accountPassword={}".format(self.env["password"])
+
+        login_data = appleIDstring + appIDKeystring + passwordstring
+        download_dir = os.path.join(
+            self.env["RECIPE_CACHE_DIR"], "downloads"
+        )
+        filename = "login_data"
+        # create download_dir if needed
+        if not os.path.exists(download_dir):
+            try:
+                os.makedirs(download_dir)
+            except OSError, err:
+                raise ProcessorError(
+                    "Can't create %s: %s" % (download_dir, err.strerror))
+        self.output("Writing data to file")
+        self.env['data_pathname'] = os.path.join(download_dir, filename)
+        with open(self.env['data_pathname'], "w") as f:
+            f.write(login_data)
+
+
+if __name__ == "__main__":
+    PROCESSOR = AppleDataGatherer()
+    PROCESSOR.execute_shell()
---
+++ Xcode/AppleURLSearcher.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python
+#
+# Copyright 2015 Greg Neagle
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""See docstring for URLDownloader class"""
+
+import os.path
+import re
+import json
+import posixpath
+import urlparse
+from distutils import version
+from types import StringType
+
+from autopkglib import Processor, ProcessorError
+
+
+__all__ = ["AppleURLSearcher"]
+
+class MunkiLooseVersion(version.LooseVersion):
+    '''Subclass version.LooseVersion to compare things like
+    "10.6" and "10.6.0" as equal'''
+    def __init__(self, vstring=None):
+        if vstring is None:
+            # treat None like an empty string
+            self.parse('')
+        if vstring is not None:
+            if isinstance(vstring, unicode):
+                # unicode string! Why? Oh well...
+                # convert to string so version.LooseVersion doesn't choke
+                vstring = vstring.encode('UTF-8')
+            self.parse(str(vstring))
+    def _pad(self, version_list, max_length):
+        """Pad a version list by adding extra 0
+        components to the end if needed"""
+        # copy the version_list so we don't modify it
+        cmp_list = list(version_list)
+        while len(cmp_list) < max_length:
+            cmp_list.append(0)
+        return cmp_list
+    def __cmp__(self, other):
+        if isinstance(other, StringType):
+            other = MunkiLooseVersion(other)
+        max_length = max(len(self.version), len(other.version))
+        self_cmp_version = self._pad(self.version, max_length)
+        other_cmp_version = self._pad(other.version, max_length)
+        return cmp(self_cmp_version, other_cmp_version)
+
+
+class AppleURLSearcher(Processor):
+    """Downloads a URL to the specified download_dir using curl."""
+    description = __doc__
+    input_variables = {
+        'result_output_var_name': {
+            'description': ('The name of the output variable that is returned '
+                            'by the match. If not specified then a default of '
+                            '"match" will be used.'),
+            'required': False,
+            'default': 'match',
+        },
+        "re_pattern": {
+            "required": True,
+            "description": "Path to download data file from AppleCookieDownloader.",
+        },
+        're_flags': {
+            'description': ('Optional array of strings of Python regular '
+                            'expression flags. E.g. IGNORECASE.'),
+            'required': False,
+        },
+
+    }
+    output_variables = {
+        'result_output_var_name': {
+            'description': (
+                'First matched sub-pattern from input found on the fetched '
+                'URL. Note the actual name of variable depends on the input '
+                'variable "result_output_var_name" or is assigned a default of '
+                '"match."')
+        }
+    }
+
+    def main(self):
+      output_var_name = self.env['result_output_var_name']
+      download_dir = os.path.join(self.env["RECIPE_CACHE_DIR"], "downloads")
+      downloads = os.path.join(download_dir, 'listDownloads')
+
+      if not os.path.exists(downloads):
+        raise ProcessorError("Missing the download data from AppleCookieDownloader")
+
+      flags = self.env.get('re_flags', {})
+
+      with open(downloads) as f:
+        data = json.load(f)
+      dl_base_url = "https://download.developer.apple.com"
+      dl_urls = []
+      for x in data['downloads']:
+        for y in x['files']:
+          dl_urls.append(dl_base_url + y['remotePath'])
+
+      flag_accumulator = 0
+      if flags:
+        for flag in flags:
+          if flag in re.__dict__:
+            flag_accumulator += re.__dict__[flag]
+
+      re_pattern = re.compile(self.env['re_pattern'], flags=flag_accumulator)
+
+      match = re_pattern.findall("\n".join(sorted(dl_urls)))
+      if not match:
+        raise ProcessorError('No match found!')
+
+      # Now we have a list of matching URLs, find the highest version
+      filenames = [
+        os.path.splitext(posixpath.basename(urlparse.urlsplit(x).path))[0] 
+        for x in match
+      ]
+      filenames.sort(key=MunkiLooseVersion)
+      self.output('Found matching item: %s' % filenames[-1])
+      full_url_match = [x for x in match if filenames[-1] in x]
+
+      if not full_url_match:
+        raise ProcessorError('No matching URL found!')
+
+      # The final entry is the highest one
+      self.output("Full URL: %s" % full_url_match[0])
+      self.env[self.env['result_output_var_name']] = full_url_match[0]
+      self.output_variables = {}
+      self.output_variables[self.env['result_output_var_name']] = full_url_match[0]
+
+
+if __name__ == "__main__":
+    PROCESSOR = AppleURLSearcher()
+    PROCESSOR.execute_shell()
