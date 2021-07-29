376e6a024601a65fdf293b0bb9bf841f23c70612
Robert Phillips <rphillip@nrao.edu>
2021-07-29 11:47:05 -0400

    Update configparser code for py3
    Removed python2-only ConfigParser code in favor of python3 configparser, even managed to steal from the same stackoverflow question (but from a python3-compatible answer). Initial testing produces working results with the current version of SQLDeveloper supplied as a .zip. No idea if the minor changes are kosher with the python codestyle contributing requirements. Also, sorry for the earlier [deleted] PR.

--- SQLDeveloper/SQLDeveloperVersioner.py
+++ SQLDeveloper/SQLDeveloperVersioner.py
@@ -19,29 +19,12 @@
 
 from __future__ import absolute_import
 
-import ConfigParser
+import configparser
 import os.path
 
 from autopkglib import Processor, ProcessorError
 
 __all__ = ["SQLDeveloperVersioner"]
-
-
-# this code stolen directly from
-# http://stackoverflow.com/questions/2819696/parsing-properties-file-in-python/2819788#2819788
-class FakeSecHead(object):
-    def __init__(self, fp):
-        self.fp = fp
-        self.sechead = "[properties]\n"
-
-    def readline(self):
-        if self.sechead:
-            try:
-                return self.sechead
-            finally:
-                self.sechead = None
-        else:
-            return self.fp.readline()
 
 
 class SQLDeveloperVersioner(Processor):
@@ -62,15 +45,25 @@ class SQLDeveloperVersioner(Processor):
         # the app's Info.plist.  Instead, the actual version is buried deep
         # inside in a file called "version.properties". Thanks, Oracle.
         # You know, I was a having a pretty good day up until now...
+
+        # this code stolen from Oscar de Groot's answer at:
+        # https://stackoverflow.com/questions/2819696/parsing-properties-file-in-python
+        def add_section_header(properties_file, header_name):
+            yield '[{}]\n'.format(header_name)
+            for line in properties_file:
+                yield line
+
         relative_path = (
             "Contents/Resources/sqldeveloper/sqldeveloper/bin/version.properties"
         )
         file_path = os.path.join(self.env["app_path"], relative_path)
-        # this code stolen directly from
-        # http://stackoverflow.com/questions/2819696/parsing-properties-file-in-python/2819788#2819788
-        cp = ConfigParser.SafeConfigParser()
+        # this code stolen from Oscar de Groot's answer at:
+        # https://stackoverflow.com/questions/2819696/parsing-properties-file-in-python
+
+        file = open(file_path, encoding="utf_8")
+        cp = configparser.ConfigParser()
         try:
-            cp.readfp(FakeSecHead(open(file_path)))
+            cp.read_file(add_section_header(file, 'properties'), source=file_path)
         except IOError as err:
             raise ProcessorError(err)
         self.env["version"] = cp.get("properties", "ver_full")
